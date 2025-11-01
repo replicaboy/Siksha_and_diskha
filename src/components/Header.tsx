128fff3965931fbc080eaae98bf9778f65070e4c
Hariom Thakur <HARIKRDBG121@GMAIL.COM>
2025-11-01 20:26:11 +0530

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -25,7 +25,7 @@ export default function Header() {
   return (
     <header className="sticky top-0 z-50 bg-white border-b border-gray-200 shadow-sm">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex justify-between items-center h-16">
+        <div className="flex justify-between items-center h-20"> {/* h-16 को h-20 से बदला */}
           {/* Logo और नाम */}
           <Link
             href="/"
